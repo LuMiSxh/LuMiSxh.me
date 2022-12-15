5862e39958114dada3dd4f5090662b4f5e63b92b
LuMiSxh <schmidt.lucamichael@icloud.com>
2022-12-15 15:44:38 +0100

    initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,8 @@
 module.exports = {
   root: true,
-  // This tells ESLint to load the config from the package `eslint-config-custom`
   extends: ["custom"],
   settings: {
-    next: {
+    svelte: {
       rootDir: ["apps/*/"],
     },
   },
