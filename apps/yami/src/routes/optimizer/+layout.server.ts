f047d95c289c6b5128bc3d016da7193b747350ae
Luca M. Schmidt <b71f1f@icloud.com>
2023-03-21 12:16:11 +0100

    Update +layout.server.ts

--- apps/yami/src/routes/optimizer/+layout.server.ts
+++ apps/yami/src/routes/optimizer/+layout.server.ts
@@ -7,7 +7,7 @@ export const load = (async ({ cookies, fetch }) => {
 	if (!access_session) {
 		throw redirect(303, `${SECRET_PATH}/api/auth/bungie-login`);
 	}
-console.log(access_session)
+console.log(access_session, "\n\n")
 	if (!cookies.get("AccessSession")) {
 		throw error(500, "The cookie for 'AccessSession' is not available.")
 	}
@@ -16,7 +16,7 @@ console.log(access_session)
 	if (access_request.status !== 200) {
 		throw error(500, access_request.statusText);
 	}
-console.log(access_request)
+console.log(access_request, "\n\n")
 	const character_request = await fetch(`${SECRET_PATH}/api/obtain/characters`);
 	if (character_request.status !== 200) {
 		throw error(500, character_request.statusText);
@@ -26,7 +26,7 @@ console.log(access_request)
 	if (level_request.status !== 200) {
 		throw error(500, level_request.statusText);
 	}
-console.log(level_request)
+console.log(level_request, "\n\n")
 	return {
 		access: { ...(await access_request.json()) },
 		character: { ...(await character_request.json()) },
